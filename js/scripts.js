6388a88a0cfa4187bb0348d98730d7aaa60cc45e
supern0va1977 <85573435+supern0va1977@users.noreply.github.com>
2021-06-18 16:51:48 -0500

    added folders and files
    start of new project

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 // the below is only a test
 /* the below
 is only a test */
-let myName = 'Bob';
-document.write(myName);
-myName = 'john Doe';
-document.write(myName);
+
+alert('Hello world');
+
+let favoriteFood = 'sushi'
+document.write(favoriteFood)
 
 //lests do some math shal we
 
 let avg = ((10+20+5+15)/(2));
 document. write(avg);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = ((doubleSize * 2) - (size / 2));
-document.write(minSize);
