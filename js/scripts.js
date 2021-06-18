43a7136c806ae20d53872ae0e63bbec02f730c26
supern0va1977 <85573435+supern0va1977@users.noreply.github.com>
2021-06-18 16:31:38 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// the below is only a test
+/* the below
+is only a test */
+let myName = 'Bob';
+document.write(myName);
+myName = 'john Doe';
+document.write(myName);
+
+//lests do some math shal we
+
+let avg = ((10+20+5+15)/(2));
+document. write(avg);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = ((doubleSize * 2) - (size / 2));
+document.write(minSize);
