b994f99a1f04f50e0bc25383df54bdef7ba251ab
supern0va1977 <85573435+supern0va1977@users.noreply.github.com>
2021-07-29 00:00:22 -0500

    added IIFE
    IIFE added to code

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,51 @@
 // Pokémon data , height is in Meters
 
-let pokemonList = [
-  {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire', 'flying']
-  },
-  {
-    name:'Beedrill',
-    height: 1.0,
-    types: ['bug', 'poison']
-  },
-  {
-    name:'Nidoking',
-    height: 1.4,
-    types: ['poison', 'ground']
-  },
-  {
-    name:'Ivysaur',
-    height: 1.1,
-    types: ['grass', 'poison']
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['fire', 'flying']
+    },
 
-pokemonList.forEach(function(pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + ' meters Tall ' +
-  pokemon.types + ' => ability   ' );
-});
+    {
+      name:'Beedrill',
+      height: 1.0,
+      types: ['bug', 'poison']
+    },
+
+    {
+      name:'Nidoking',
+      height: 1.4,
+      types: ['poison', 'ground']
+    },
+
+    {
+      name:'Ivysaur',
+      height: 1.1,
+      types: ['grass', 'poison']
+    },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && Object.keys(pokemon).includes('name') && Object.keys(pokemon).includes('type') && Object.keys(pokemon).includes('height')) {
+      pokemonList.push(pokemon);
+    }
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    
+  };
+}) ();
 
 // shows the pokemonList
+pokemonList.forEach(function(pokemon) {
+  (pokemon.name + ' is ' + pokemon.height + ' meters Tall ' +
+  pokemon.types + ' => ability   ' );
+});
